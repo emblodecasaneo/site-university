6c8e6035c50800802f50a58fb25f5a6ea1e07633
mbolodecasaneo <mbolo@casaneo.io>
2024-07-18 10:03:59 +0200

    updates

--- src/template/navbar.php
+++ src/template/navbar.php
@@ -3,9 +3,9 @@
             <div class="navigation">
                 <ul>
                     <li><a href="index.php?page=home" class="<?= ($_GET['page'] ?? 'home') === 'home' ? 'active' : '' ?>">Home</a></li>
-                    <li><a href="index.php?page=course" class="<?= ($_GET['page'] ?? 'course') === 'course' ? 'active' : '' ?>">Courses</a></li>
-                    <li><a href="index.php?page=about" class="<?= ($_GET['page'] ?? 'about') === 'about' ? 'active' : '' ?>">About us</a></li>
-                    <li><a href="index.php?page=contact" class="<?= ($_GET['page'] ?? 'contact') === 'contact' ? 'active' : '' ?>">Contact</a></li>
+                    <li><a href="index.php?page=course" class="<?= ($_GET['page'] ?? 'home') === 'course' ? 'active' : '' ?>">Courses</a></li>
+                    <li><a href="index.php?page=about" class="<?= ($_GET['page'] ?? 'home') === 'about' ? 'active' : '' ?>">About us</a></li>
+                    <li><a href="index.php?page=contact" class="<?= ($_GET['page'] ?? 'home') === 'contact' ? 'active' : '' ?>">Contact</a></li>
                 </ul>
             </div>
 </nav>
